653bcdc2151da8aa4a7c1f23d40e97e54b19be7f
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2023-05-26 22:56:26 +0600

    Good pyramid added

--- pyramid.py
+++ pyramid.py
@@ -1,6 +1,7 @@
+from time import sleep
 import pyautogui
-n = int(input(""))  
-for i in range(0, n):  
-    pyautogui.keyDown('shift')
-    pyautogui.write("# " * (i+1), interval=0.25)       
-    print()
+n = int(input())  
+#sleep(3)
+for i in range(1,n+1):  
+    pyautogui.write("#"*i, interval=0.25)  
+    pyautogui.press('enter')
